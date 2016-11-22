b07967b6c26ad0c00e951bbc4df896bf58856139
apanfil <apanfil@css.edu>
2016-11-22 11:06:54 -0600

    Added a user repository

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,12 +13,23 @@ namespace ConsoleSample
         static void Main(string[] args)
         {
 
-            EntityRepository entityRepository = new EntityRepository();
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetById(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
+
+//user repository
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetById(Guid.NewGuid());
+
+            myUser.Name = "Tom";
+            myUser.EmailAddress = "apanfil@css.edu";
+            Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
+            Console.WriteLine("New entity Name: " + myEntity.Name);
+            Console.WriteLine("New entity email: " + myUser.EmailAddress;
+
 
         }
     }
---
+++ SubtypeCovariance/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtypeCovariance
+{
+
+        public interface IUserRepository<TUser>
+        where TUser : User
+    {
+        TUser GetById(Guid id);
+    }
+}
